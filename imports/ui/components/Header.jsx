d91e2a9641da4bfd10055814814352de4d9b70f0
Juan Fernandez <juan.e.fernandez.suazo@gmail.com>
2017-06-11 18:51:56 -0400

    arreglo pages2

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -6,16 +6,14 @@ import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 export default class Header extends React.Component {
   render() {
     return (
-    <Navbar inverse collapse>
+    <Navbar inverse>
       <Navbar.Header>
         <Navbar.Brand>
-          <a href="#">ChatWeb</a>
+          <a href="/">ChatWeb</a>
         </Navbar.Brand>
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav>
-          <NavItem divider />
-          <NavItem eventKey={1} href="/">Home</NavItem>
           <NavItem eventKey={2} href="/RoomChat">RoomChat</NavItem>
         </Nav>
         <Nav pullRight>
--- imports/ui/pages/RoomChat.jsx
+++ imports/ui/pages/RoomChat.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import { Tab, Row, Col, Nav, NavItem } from 'react-bootstrap';
+
 function RoomChat(){
     return(
-      <div>
-        <h1>aqui va el chat</h1>
-      </div>
+      <Tab.Container id="list_users" defaultActiveKey="first">
+        <Row className="clearfix">
+          <Col sm={4}>
+            <Nav bsStyle="pills" stacked>
+              <NavItem eventKey="first">
+                Primer Usuario
+              </NavItem>
+              <NavItem eventKey="second">
+                Segundo Usuario
+              </NavItem>
+            </Nav>
+          </Col>
+          <Col sm={8}>
+            <Tab.Content animation>
+              <Tab.Pane eventKey="first">
+                Tab 1 content
+              </Tab.Pane>
+              <Tab.Pane eventKey="second">
+                Tab 2 content
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     );
 }
 export default RoomChat;
